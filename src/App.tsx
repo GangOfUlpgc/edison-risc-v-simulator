074bdfd3797b318217d0bdf489c66c800dae75f2
josericardopenase <josericardopenase@gmail.com>
2023-06-01 19:00:28 +0100

    Alu test

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,21 @@ import { Constant } from "./engines/vega/src/core/components/constant";
 import { Register } from "./engines/vega/src/core/components/register";
 import { Alu32 } from "./engines/vega/src/core/components/alu32";
 
-const register = new Register([], {
-  "0": "addi $t0, $zero, 0",
-  "100": "mult $t0, $t0",
-  "1000": "lw $t0, $t0",
-  "1100": "ld $t0, $t0",
-});
+const alu1 = new Alu32([
+  new Constant("0"),
+  new Constant("1"),
+  new Constant("010"),
+]);
 
-console.log(register.state);
-let pc = "0";
+const alu2 = new Alu32([
+  new Constant("0"),
+  new Constant("1"),
+  new Constant("010"),
+]);
 
-for (let i = 0; i < 4; i++) {
-  const constant = new Constant(pc);
-  const alu32 = new Alu32([]);
+const alu3 = new Alu32([alu1, alu2, new Constant("010")]);
 
-  register.setInputs([constant, new Constant("0"), new Constant("0")]);
-  alu32.setInputs([constant, new Constant("100"), new Constant("010")]);
-
-  console.log(register.execute());
-  pc = alu32.execute();
-}
+console.log(alu3.execute());
 
 function App() {
   return <RouterProvider router={AppRouter}></RouterProvider>;
