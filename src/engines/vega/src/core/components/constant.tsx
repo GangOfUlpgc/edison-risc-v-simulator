267fcb3a3b73034869b68eda71c172758f23c90c
josericardopenase <josericardopenase@gmail.com>
2023-06-01 19:00:01 +0100

    Vega: implemented core

---
+++ src/engines/vega/src/core/components/constant.tsx
@@ -0,0 +1,13 @@
+import { DatapathComponent } from "./base";
+
+export class Constant extends DatapathComponent {
+  value: string;
+  constructor(value: string) {
+    super([]);
+    this.value = value;
+  }
+
+  public execute(): string {
+    return this.value;
+  }
+}
