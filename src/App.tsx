464968c6fe0684c00e04648bd5fc756822f9497c
Daniel Hernandez <danielersan@gmail.com>
2023-06-02 21:57:16 +0100

    instruction decoded finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { Alu32 } from "./engines/vega/src/core/modular/alu32";
 import { Constant } from "./engines/vega/src/core/modular/constant";
 import { Multiplexor } from "./engines/vega/src/core/modular/multiplexor";
 import { Register } from "./engines/vega/src/core/modular/register";
+import { decodeInstruction } from "./engines/vega/src/core/decode";
+
+
+const decoded = decodeInstruction("add x1, x2, x3")
+
+console.log(decoded)
 
 function App() {
   return <RouterProvider router={AppRouter}></RouterProvider>;
