671435b133e5f4920707d43415fe7f16acb175e3
josericardopenase <josericardopenase@gmail.com>
2023-06-02 20:23:02 +0100

    Finished cpu state

---
+++ src/engines/vega/src/core/mem/ram.ts
@@ -0,0 +1,35 @@
+import { CPUMem } from "../state";
+
+interface RAMNext {
+  addr: number;
+  writeData: number;
+  memRead: number;
+  memWrite: number;
+}
+
+export class RAM {
+  read(addr: number) {
+    if (addr % 4 !== 0) throw new Error(`Invalid register address: ${addr}`);
+    return CPUMem.getState().ram[addr];
+  }
+
+  write(addr: number, value: number) {
+    if (addr % 4 !== 0) throw new Error(`Invalid register address: ${addr}`);
+    CPUMem.setState((state) => ({
+      ram: {
+        ...state.ram,
+        [addr]: value,
+      },
+    }));
+  }
+
+  next({ addr, writeData, memRead, memWrite }: RAMNext) {
+    if (memRead) {
+      return this.read(addr);
+    }
+    if (memWrite) {
+      this.write(addr, writeData);
+    }
+    return 0;
+  }
+}
