5a42c2a059ee8ef5760440b1d02ed39c175e43d6
Willy <thewillyeb@gmail.com>
2023-06-09 20:21:15 +0100

    Created datamem, alu, mux and pc components

--- src/engines/punk/src/core/nodes/DataMem.tsx
+++ src/engines/punk/src/core/nodes/DataMem.tsx
@@ -1,5 +1,20 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Handle, Position } from "reactflow";
+
+const addressStyle = { top: 20 }
+const writeDataStyle = { top: 80 }
+
 
 export default function DataMem() {
-  return <div>DataMem</div>;
+  return (
+    <Box backgroundColor="gray.200" position="relative" height="100">
+      <Box padding="2" py="6">Data memory</Box>
+      <Handle type="source" position={Position.Left} style={addressStyle} id="address" />
+      <Handle type="source" position={Position.Left} style={writeDataStyle} id="writeData" />
+      <Handle type="source" position={Position.Top} id="memRead" />
+      <Handle type="source" position={Position.Bottom} id="memWrite" />
+      <Handle type="source" position={Position.Right} id="readData" />
+    </Box>
+  )
 }
--- src/engines/punk/src/core/nodes/Multiplexor.tsx
+++ src/engines/punk/src/core/nodes/Multiplexor.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { Handle, Position } from "reactflow";
 
 export default function Multiplexor() {
-  const handleStyle = { top: 20 };
+  const firstInputStyle = { top: 20 };
+  const secondInputStyle = { top: 50 }
 
   return (
-    <Box backgroundColor="gray.200" position="relative">
-      <Box padding="2">Mux</Box>
-      <Handle type="source" position={Position.Left} style={handleStyle} id="input1" />
-      <Handle type="source" position={Position.Left} id="input2" />
+    <Box backgroundColor="gray.200" position="relative" width="50px">
+      <Box px="2" py="6" fontSize="0.8rem" textAlign="center">Mux</Box>
+      <Handle type="source" position={Position.Left} style={firstInputStyle} id="input1" />
+      <Handle type="source" position={Position.Left} style={secondInputStyle} id="input2" />
       <Handle type="source" position={Position.Bottom} id="selector" />
       <Handle type="source" position={Position.Right} id="output" />
     </Box>
