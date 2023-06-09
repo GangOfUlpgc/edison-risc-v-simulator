24337f488daf19252964c52d01c1c8a5bb4561b0
Willy <thewillyeb@gmail.com>
2023-06-09 17:54:48 +0100

    Made adder component design

--- src/engines/punk/src/core/nodes/PC.tsx
+++ src/engines/punk/src/core/nodes/PC.tsx
@@ -1,5 +1,14 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Handle, Position } from "reactflow";
+
 
 export default function PC() {
-  return <div>PC</div>;
+  return (
+    <Box backgroundColor="gray.200" position="relative">
+      <Box padding="2" py="6">PC</Box>
+      <Handle type="source" position={Position.Left} id="input" />
+      <Handle type="source" position={Position.Right} id="output" />
+    </Box>
+  )
 }
