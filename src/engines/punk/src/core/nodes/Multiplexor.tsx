cd7a636cfad70391b6c7d990a350b5b3b7d59bd6
Willy <thewillyeb@gmail.com>
2023-06-10 03:42:55 +0100

    Datapath with all nodes and edges done

--- src/engines/punk/src/core/nodes/Multiplexor.tsx
+++ src/engines/punk/src/core/nodes/Multiplexor.tsx
@@ -7,8 +7,8 @@ export default function Multiplexor() {
   const secondInputStyle = { top: 50 }
 
   return (
-    <Box backgroundColor="gray.200" position="relative" width="50px">
-      <Box px="2" py="6" fontSize="0.8rem" textAlign="center">Mux</Box>
+    <Box backgroundColor="gray.200" position="relative" width="30px">
+      <Box px="2" py="6" fontSize="0.4rem" textAlign="center">Mux</Box>
       <Handle type="target" position={Position.Left} style={firstInputStyle} id="input1" />
       <Handle type="target" position={Position.Left} style={secondInputStyle} id="input2" />
       <Handle type="target" position={Position.Bottom} id="selector" />
--- src/engines/punk/src/datapath/riscv32i.ts
+++ src/engines/punk/src/datapath/riscv32i.ts
@@ -2,33 +2,33 @@ import { Edge, Node, Position } from "reactflow";
 
 const nodes: Node[] = [
   {
-    id: "muxAlu1",
-    position: { x: 600, y: 150 },
+    id: "muxAlu",
+    position: { x: 750, y: 300 },
     type:"mux",
     data: { label: "MUX" },
   },
   {
-    id: "muxAlu2",
-    position: { x: 750, y: 150 },
+    id: "muxWb",
+    position: { x: 1600, y: 500 },
     type:"mux",
     data: { label: "MUX" },
   },
   {
-    id: "muxAlu3",
-    position: { x: 600, y: 350 },
+    id: "muxPc",
+    position: { x: -400, y: 300 },
     type:"mux",
     data: { label: "MUX" },
   },
   {
-    id: "muxAlu4",
-    position: { x: 750, y: 350 },
-    type:"mux",
-    data: { label: "MUX" },
-  },
-  {
-    id: "Adder",
+    id: "adder",
     type: "adder",
     position: { x: -100, y: -100 },
+    data: { label: "Add" },
+  },
+  {
+    id: "adderIdex",
+    type: "adder",
+    position: { x: 800, y: 50 },
     data: { label: "Add" },
   },
   {
@@ -46,7 +46,7 @@ const nodes: Node[] = [
   {
     id: "dataMemory",
     type: "datamem",
-    position: { x: -200, y: -200 },
+    position: { x: 1200, y: 250 },
     data: { label: "DataMem" },
   },
   {
@@ -65,35 +65,68 @@ const nodes: Node[] = [
   {
     id: "ifid",
     type: "bufferifid",
-    position: {x: 50, y: 150},
+    position: {x: 50, y: 100},
     data: {label: "buffer if/id"},
   },
   {
     id: "idex",
     type: "bufferidex",
-    position: {x: 450, y: 150},
+    position: {x: 500, y: 100},
     data: {label: "buffer id/ex"},
   },
   {
     id: "exmem",
     type: "bufferexmem",
-    position: {x: 1000, y: 150},
+    position: {x: 1000, y: 100},
     data: {label: "buffer ex/mem"},
-  }
+  },
+  {
+    id: "memwb",
+    type: "buffermemwb",
+    position: {x: 1400, y: 100},
+    data: {label: "buffer mem/wb"},
+  },
+
+
 ];
 const edges: Edge[] = [
   { id: "e1-1", source: "pc", target: "instructionMemory", label: "0x00000000", sourceHandle:"output", targetHandle: "address" },
-  { id: "e1-2", source: "instructionMemory", target: "ifid", sourceHandle:"readInstr", targetHandle: "input2" },
-  { id: "e1-3", source: "ifid", target: "registers", sourceHandle:"output2", targetHandle: "readReg1"},
-  { id: "e1-4", source: "ifid", target: "registers", sourceHandle:"output2", targetHandle: "readReg2"},
-  { id: "e1-5", source: "registers", target: "idex", sourceHandle:"readData1", targetHandle: "input1"},
-  { id: "e1-6", source: "registers", target: "idex", sourceHandle:"readData2", targetHandle: "input2"},
-  {id: "e1-7", source: "idex", target: "muxAlu1", sourceHandle:"output1", targetHandle: "input1"},
-  {id: "e1-8", source: "idex", target: "muxAlu3", sourceHandle:"output2", targetHandle: "input1"},
-  {id: "e1-9", source: "muxAlu1", target: "muxAlu2", sourceHandle:"output", targetHandle: "input1"},
-  {id: "e1-10", source: "muxAlu3", target: "muxAlu4", sourceHandle:"output", targetHandle: "input1"},
-  {id: "e1-11", source: "muxAlu2", target: "alu", sourceHandle:"output", targetHandle: "input1"},
-  {id: "e1-12", source: "muxAlu4", target: "alu", sourceHandle:"output", targetHandle: "input2"},
+  { id: "e1-2", source: "pc", target: "adder", sourceHandle:"output", targetHandle: "input1" },
+  { id: "e1-3", source: "pc", target: "ifid", sourceHandle:"output", targetHandle: "input1" },
+  { id: "e1-4", source: "adder", target: "muxPc", sourceHandle:"output", targetHandle: "input1" },
+  { id: "e1-5", source: "muxPc", target: "pc", sourceHandle:"output", targetHandle: "input" },
+  { id: "e1-6", source: "instructionMemory", target: "ifid", sourceHandle:"readInstr", targetHandle: "input2" },
+
+  { id: "e2-1", source: "ifid", target: "registers", sourceHandle:"output2", targetHandle: "readReg1"},
+  { id: "e2-2", source: "ifid", target: "registers", sourceHandle:"output2", targetHandle: "readReg2"},
+  { id: "e2-3", source: "ifid", target: "idex", sourceHandle:"output1", targetHandle: "input1"},
+  { id: "e2-4", source: "ifid", target: "idex", sourceHandle:"output2", targetHandle: "input5", label: "instruction"},
+  { id: "e2-5", source: "ifid", target: "idex", sourceHandle:"output2", targetHandle: "input4"},
+  { id: "e2-6", source: "registers", target: "idex", sourceHandle:"readData1", targetHandle: "input2"},
+  { id: "e2-7", source: "registers", target: "idex", sourceHandle:"readData2", targetHandle: "input3"},
+
+  {id: "e3-1", source: "idex", target: "alu", sourceHandle:"output2", targetHandle: "input1"},
+  {id: "e3-4", source: "idex", target: "adderIdex", sourceHandle: "output1", targetHandle: "input1"},
+  {id: "e3-2", source: "idex", target: "muxAlu", sourceHandle:"output3", targetHandle: "input1"},
+  {id: "e3-3", source: "idex", target: "exmem", sourceHandle:"output3", targetHandle: "input3"},
+  {id: "e3-4", source: "idex", target: "muxAlu", sourceHandle:"output4", targetHandle: "input2"},
+  {id: "e3-5", source: "idex", target: "adderIdex", sourceHandle:"output4", targetHandle: "input2"},
+  {id: "e3-6", source: "muxAlu", target: "alu", sourceHandle: "output", targetHandle: "input2"},
+  {id: "e3-7", source: "alu", target: "exmem", sourceHandle: "output", targetHandle: "input2"},
+  {id: "e3-8", source: "adderIdex", target: "exmem", sourceHandle: "output", targetHandle: "input1"},
+  {id: "e3-9", source: "idex", target: "exmem", sourceHandle: "output5", targetHandle: "input4"},
+  
+  {id: "e4-1", source: "exmem", target: "muxPc", sourceHandle: "output1", targetHandle: "input2"},
+  {id: "e4-2", source: "exmem", target: "dataMemory", sourceHandle: "output2", targetHandle: "address"},
+  {id: "e4-3", source: "exmem", target: "dataMemory", sourceHandle: "output3", targetHandle: "writeData"},
+  {id: "e4-5", source: "exmem", target: "memwb", sourceHandle: "output2", targetHandle: "input2"},
+  {id: "e4-6", source: "exmem", target: "memwb", sourceHandle: "output4", targetHandle: "input3"},
+  {id: "e4-7", source: "dataMemory", target: "memwb", sourceHandle: "readData", targetHandle: "input1"},
+  
+  {id: "e5-1", source: "memwb", target: "muxWb", sourceHandle: "output1", targetHandle: "input1"},
+  {id: "e5-2", source: "memwb", target: "muxWb", sourceHandle: "output2", targetHandle: "input2"},
+  {id: "e5-3", source: "memwb", target: "registers", sourceHandle: "output3", targetHandle: "writeReg"},
+  {id: "e5-4", source: "muxWb", target: "registers", sourceHandle: "output", targetHandle: "writeData", label: "WB Data"},
 ];
 
 export default {
