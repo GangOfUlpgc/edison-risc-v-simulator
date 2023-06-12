8ac067701371af2911a6bcec10c59f19c0fde4f1
Willy <thewillyeb@gmail.com>
2023-06-12 18:52:01 +0100

    Minor changes on segmented bar

--- src/engines/punk/src/datapath/SegmentedBar.tsx
+++ src/engines/punk/src/datapath/SegmentedBar.tsx
@@ -4,23 +4,23 @@ import React from 'react'
 export default function SegmentedBar() {
     return (
         <Box display="flex" width="100%" flexDirection="row" borderRadius="10px" bgColor="gray.100" mb="1" color="gray.600">
-            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="2" bgColor="#FEFCBF">
+            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="1" bgColor="#FEFCBF">
                 <Text>Fetch</Text>
                 <Text>0x00000000</Text>
             </Box>
-            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="2" bgColor="#bee3f8">
+            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="1" bgColor="#bee3f8">
                 <Text>Decode</Text>
                 <Text>0x00000000</Text>
             </Box>
-            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="2" bgColor="#C6F6D5">
+            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="1" bgColor="#C6F6D5">
                 <Text>Execute</Text>
                 <Text>0x00000000</Text>
             </Box>
-            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="2" bgColor="#E9D8FD" >
+            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="1" bgColor="#E9D8FD" >
                 <Text>Memory</Text>
                 <Text>0x00000000</Text>
             </Box>
-            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="2" bgColor="#FED7D7" >
+            <Box justifyContent="center" textAlign="center" flexBasis="25%" borderRadius="10px" margin="1" bgColor="#FED7D7" >
                 <Text>WriteBack</Text>
                 <Text>0x00000000</Text>
             </Box>
