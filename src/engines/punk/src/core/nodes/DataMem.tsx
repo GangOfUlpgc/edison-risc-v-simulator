e158cf726708c540ea66d53cae081756e359904e
josericardopenase <josericardopenase@gmail.com>
2023-06-12 19:19:13 +0100

    HeaderControl

--- src/engines/punk/src/core/nodes/DataMem.tsx
+++ src/engines/punk/src/core/nodes/DataMem.tsx
@@ -2,13 +2,15 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import { Handle, Position } from "reactflow";
 import ValuePopover from "../components/ValuePopover";
+import { rv32i } from "../../../../../cpus";
 
 const addressStyle = { top: 20 };
 const writeDataStyle = { top: 80 };
 
 export default function DataMem() {
+  const ins = rv32i.useState((state) => state.pipeline.MEM.cpumeta.ReadData);
   return (
-    <ValuePopover value="0x00000000">
+    <ValuePopover value={"0x" + (ins ?? "0").toString(16)}>
       <Box backgroundColor="gray.200" position="relative" height="100">
         <Box padding="2" py="6">
           Data memory
