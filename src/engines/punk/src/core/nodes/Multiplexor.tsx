f65d703be9e17ec969486a501a902fa69d65aac1
josericardopenase <josericardopenase@gmail.com>
2023-06-12 19:19:13 +0100

    HeaderControl

--- src/engines/punk/src/core/nodes/Multiplexor.tsx
+++ src/engines/punk/src/core/nodes/Multiplexor.tsx
@@ -5,17 +5,28 @@ import ValuePopover from "../components/ValuePopover";
 
 export default function Multiplexor() {
   const firstInputStyle = { top: 20 };
-  const secondInputStyle = { top: 50 }
+  const secondInputStyle = { top: 50 };
 
   return (
     <Box backgroundColor="gray.200" position="relative" width="30px">
-      <Box px="2" py="6" fontSize="0.4rem" textAlign="center">Mux</Box>
-      <Handle type="target" position={Position.Left} style={firstInputStyle} id="input1" />
-      <Handle type="target" position={Position.Left} style={secondInputStyle} id="input2" />
+      <Box px="2" py="6" fontSize="0.4rem" textAlign="center">
+        Mux
+      </Box>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={firstInputStyle}
+        id="input1"
+      />
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={secondInputStyle}
+        id="input2"
+      />
       <Handle type="target" position={Position.Bottom} id="selector" />
-      <ValuePopover value="0x00000000">
-        <Handle type="source" position={Position.Right} id="output" />
-      </ValuePopover>
+
+      <Handle type="source" position={Position.Right} id="output" />
     </Box>
-  )
+  );
 }
